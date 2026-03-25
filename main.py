6ad5223016a679d7aa7c0c83129cfb2b5ea165f8
EllisSK <ellissinclairkent@gmail.com>
2026-03-25 15:51:37 +0000

    Updated unit hydrograph methods

--- main.py
+++ main.py
@@ -14,18 +14,9 @@ def main():
 
     #wrtie_jackknife_report(data, 100, "exports/jack_knife.txt")
 
-    catchment_parameters = parse_feh_xml_to_dataframe("data/FEH_Catchment_Descriptors.xml")
+    design_storm = calculate_design_storm("data/FEH_Catchment_Descriptors.xml")
 
-    #print(catchment_parameters)
-
-    profile = calculate_storm_profile(0.001, 7, 1)
-
-    print(profile, sum(profile))
-
-    fig = px.line(profile)
-
-    fig.show()
-
+    print(design_storm)
 
 if __name__ == "__main__":
     main()
--- src/estimation/hydrograph.py
+++ src/estimation/hydrograph.py
@@ -38,17 +38,19 @@ def parse_feh_xml_to_dataframe(xml_file_path):
         value_name="Value"
     )
 
+    df_long = df_long.set_index("Catchment Descriptor")
+
     return df_long
 
 def interpolate_ddf_2022(xml_file_path, return_period, duration):
     df_parent = pd.read_xml(xml_file_path, xpath=".//CatchmentAverageDDF2022Values")
-    rp_str = df_parent["ReturnPeriods"].iloc
+    rp_str = df_parent["ReturnPeriods"].iloc[0]
     return_periods = np.array([float(x.strip()) for x in str(rp_str).split(",")])
     
     df_depths = pd.read_xml(xml_file_path, xpath=".//CatchmentAverageDDF2022Values/Depths")
     durations = df_depths["duration"].astype(float).values
     
-    depth_col = [col for col in df_depths.columns if col != "duration"]
+    depth_col = [col for col in df_depths.columns if col != "duration"][0]
     depths_matrix = np.array([
         [float(x.strip()) for x in str(val).split(",")]
         for val in df_depths[depth_col]
@@ -85,32 +87,141 @@ def calculate_design_storm_precipitation(propwet, dplbar, urbext1990, dpsbar, sa
 
     return rddf * arf * scf
 
-def calculate_storm_profile(urbext1990, D, Dt):
-    print(urbext1990)
-    if urbext1990 < 0.125:
-        a = 0.06
-        b = 1.026
-    else:
-        a = 0.1
-        b = 0.815
-
-    def intensity_func(t, a, b):
-        x = 2 * np.abs(t - 0.5)
-        z = x * b
-        
-        intensity = (b * np.log(a) * np.power(a, z)) / (a - 1)
-        return intensity
+def calculate_storm_profile(D, Dt):
     
+    hardcoded_values = [
+        0.189619671,
+        0.20077609,
+        0.212579979,
+        0.225068136,
+        0.238279372,
+        0.252254619,
+        0.267037033,
+        0.282672106,
+        0.299207781,
+        0.316694568,
+        0.335185672,
+        0.354737113,
+        0.375407862,
+        0.397259969,
+        0.420358703,
+        0.444772687,
+        0.470574035,
+        0.497838496,
+        0.526645586,
+        0.557078727,
+        0.589225372,
+        0.623177133,
+        0.659029883,
+        0.696883858,
+        0.736843729,
+        0.779018648,
+        0.823522254,
+        0.870472642,
+        0.919992252,
+        0.972207692,
+        1.027249439,
+        1.085251402,
+        1.146350297,
+        1.21068476,
+        1.278394113,
+        1.349616649,
+        1.424487248,
+        1.503134017,
+        1.58567353,
+        1.672203919,
+        1.762794646,
+        1.857470845,
+        1.956188414,
+        2.058792283,
+        2.164941684,
+        2.273963814,
+        2.384529085,
+        2.493780946,
+        2.59409202,
+        2.635979223,
+        2.635979223,
+        2.59409202,
+        2.493780946,
+        2.384529085,
+        2.273963814,
+        2.164941684,
+        2.058792283,
+        1.956188414,
+        1.857470845,
+        1.762794646,
+        1.672203919,
+        1.58567353,
+        1.503134017,
+        1.424487248,
+        1.349616649,
+        1.278394113,
+        1.21068476,
+        1.146350297,
+        1.085251402,
+        1.027249439,
+        0.972207692,
+        0.919992252,
+        0.870472642,
+        0.823522254,
+        0.779018648,
+        0.736843729,
+        0.696883858,
+        0.659029883,
+        0.623177133,
+        0.589225372,
+        0.557078727,
+        0.526645586,
+        0.497838496,
+        0.470574035,
+        0.444772687,
+        0.420358703,
+        0.397259969,
+        0.375407862,
+        0.354737113,
+        0.335185672,
+        0.316694568,
+        0.299207781,
+        0.282672106,
+        0.267037033,
+        0.252254619,
+        0.238279372,
+        0.225068136,
+        0.212579979,
+        0.20077609,
+        0.189619671
+    ]
+
+  
     n_rain_bars = int(D / Dt)
 
-    half_step = (Dt / D) / 2.0
-    time_vals = np.linspace(half_step, 1.0 - half_step, n_rain_bars)
-
-    profile = intensity_func(time_vals, a, b)
-
-    profile_sum = sum(profile)
-
-    return profile / profile_sum
+    original_spacing = np.linspace(0, 1, len(hardcoded_values))
     
-def calculate_design_storm():
-    pass
+    target_spacing = np.linspace(0, 1, n_rain_bars)
+    
+    #Slightly more accurate than provided excel worksheet as values are linearly interoplated rather than rounded to nearest percentile
+    resampled_profile = np.interp(target_spacing, original_spacing, hardcoded_values)
+
+    normalised_profile = resampled_profile / np.sum(resampled_profile)
+    
+    return normalised_profile
+
+    
+def calculate_design_storm(xml_file_path):
+    Dt = 1
+
+    df = parse_feh_xml_to_dataframe(xml_file_path)
+
+    Tp = calculate_time_to_peak(df["Value"]["propwet"], df["Value"]["dplbar"], df["Value"]["urbext1990"], df["Value"]["dpsbar"])
+
+    D = calculate_duration(Tp, df["Value"]["saar"], Dt)
+
+    profile = calculate_storm_profile(D, Dt)
+
+    rddf = interpolate_ddf_2022(xml_file_path, 100, D)
+
+    arf = calculate_arf(df["Value"]["area"], D)
+
+    scf = 0.89
+
+    return profile * arf * scf * rddf
