8bf850e79956c090aeaed4cec10349908c738607
Ellis Sinclair-Kent <ellissinclairkent@gmail.com>
2026-03-26 04:38:14 +0000

    Added placeholders for shetran analysis

---
+++ src/estimation/shetran.py
@@ -0,0 +1,28 @@
+from .statistical.local import fit_gev_lmom, perform_jack_knifing, create_jackknife_plot
+
+def calculate_rainfall_statistics(series):
+    propwet = 1
+
+    tenth = 1
+    median = 1
+    ninetieth = 1
+
+
+
+def calculate_kge(obs, sim):
+    pass
+
+def analyse_shetran_performance():
+    #nomral kge
+
+    #kge over threshold
+    pass
+
+def create_sim_obs_plot():
+    pass
+
+def fit_shetran_gev():
+    pass
+
+def create_comparison_plot():
+    pass
